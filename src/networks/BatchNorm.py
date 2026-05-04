added static implementation, fixed FWT computation bug

src/networks/BatchNorm.py
@@ -22,6 +22,7 @@ class _BatchNorm(nn.Module):
         self.pi = args.pi
         self.rho = args.rho
         self.device = args.device
+        self.static = getattr(args, 'static', False)
 
         if self.affine:
             # Weight parameters
@@ -89,7 +90,7 @@ class _BatchNorm(nn.Module):
 
 
 
-        if self.training or sample:
+        if (self.training or sample) and not self.static:
             weight = self.weight.sample()
             bias = self.bias.sample()
         else:

src/networks/BayesianConvs.py
@@ -30,6 +30,7 @@ class _ConvNd(nn.Module):
         self.pi = args.pi
         self.rho = args.rho
         self.device = args.device
+        self.static = getattr(args, 'static', False)
 
 
         if transposed:
@@ -102,7 +103,7 @@ class BayesianConv2D(_ConvNd):
             # if self.use_bias:
             #     self.bias = VariationalPosterior(self.bias_mu, self.bias_rho)
 
-        if self.training or sample:
+        if (self.training or sample) and not self.static:
             weight = self.weight.sample()
             bias = self.bias.sample() if self.use_bias else None
                 

src/networks/FC.py
@@ -17,6 +17,7 @@ class BayesianLinear(nn.Module):
         self.use_bias = use_bias
         self.device = args.device
         self.rho = args.rho
+        self.static = getattr(args, 'static', False)
 
         # Variational Posterior Distributions
         self.weight_mu = nn.Parameter(torch.empty((out_features, in_features),
@@ -125,7 +126,7 @@ class BayesianLinear(nn.Module):
             # if self.use_bias:
             #     self.bias = VariationalPosterior(self.pruned_bias_mu, self.pruned_bias_rho)
 
-        if self.training or sample:
+        if (self.training or sample) and not self.static:
             weight = self.weight.sample()
             bias = self.bias.sample() if self.use_bias else None
             

src/run.py
@@ -14,7 +14,7 @@ parser.add_argument('--device',             default='mps',       type=str,   hel
 parser.add_argument('--wandb_mode',         default='online',       type=str,   help='Online or offline for wandb')
 parser.add_argument('--experiment',         default='mnist5',       type=str,   required=True,
                                             choices=['mnist2','mnist5','pmnist','cifar','mixture'])
-parser.add_argument('--trainer',           default='grow',            type=str,   help='acl')
+parser.add_argument('--train_mode',         default='grow',            type=str,   help='continual growth or static joint')
 parser.add_argument('--data_path',          default='../data/',            type=str,   help='gpu id')
 parser.add_argument('--cl_mode',            default='domain-incremental', type=str, choices=['task-incremental', 'domain-incremental'], help='Continual learning mode')
 
@@ -35,6 +35,7 @@ parser.add_argument('--sig1',               default='0.0',          type=float, 
 parser.add_argument('--sig2',               default='6.0',          type=float,   help='STD foor the 2nd prior pdf in scaled mixture Gaussian')
 parser.add_argument('--pi',                 default='0.25',         type=float,   help='weighting factor for prior')
 parser.add_argument('--arch',               default='mlp',          type=str,     help='Bayesian Neural Network architecture')
+parser.add_argument('--static',            action='store_true',    help='Use only means (no Bayesian sampling)')
 
 parser.add_argument('--resume',          default='no',            type=str,   help='resume?')
 parser.add_argument('--sti',             default=0,               type=int,   help='starting task?')
@@ -73,7 +74,7 @@ elif args.experiment=='mixture':
     from dataloaders import mixture as dataloader
 
 # Args -- Trainer
-if args.trainer=='grow':
+if args.train_mode=='grow':
     from train import trainer as trainer_module
 
 # Args -- Network
@@ -127,7 +128,7 @@ for t,ncla in taskcla[args.sti:]:
     print('Task {:2d} ({:s})'.format(t,data[t]['name']))
     print('*'*100)
 
-    if args.trainer == 'joint':
+    if args.train_mode == 'joint':
         # Get data. We do not put it to GPU
         if t==0:
             xtrain=data[t]['train']['x']
@@ -168,7 +169,7 @@ for t,ncla in taskcla[args.sti:]:
 
     # Save
     print('Save at '+args.checkpoint)
-    np.savetxt(os.path.join(args.checkpoint,'{}_{}_{}.txt'.format(args.experiment,args.trainer,args.seed)),acc,'%.5f')
+    np.savetxt(os.path.join(args.checkpoint,'{}_{}_{}.txt'.format(args.experiment,args.train_mode,args.seed)),acc,'%.5f')
 
 
 utils.print_log_acc_bwt(args, acc, lss)

src/utils.py
@@ -71,7 +71,7 @@ def save_log(taskcla, acc, lss, data, output_path):
 
 def make_directories(args):
     if args.output=='':
-        args.output = '{}_{}'.format(args.experiment,args.trainer)
+        args.output = '{}_{}'.format(args.experiment,args.train_mode)
         print (args.output)
     checkpoint = os.path.join(args.checkpoint_dir, args.output)
     if not os.path.exists(args.checkpoint_dir):
@@ -100,7 +100,7 @@ def print_log_acc_bwt(args, acc, lss):
     grow_bwt = (acc[-1] - np.diag(acc)).mean()
     print ('BWT : {:5.2f}%'.format(grow_bwt))
 
-    grow_fwt = (acc[0] - np.diag(acc)).mean()
+    grow_fwt = (acc[0,0] - np.diag(acc)).mean()
     print ('FWT : {:5.2f}%'.format(grow_fwt))
 
     print('*'*100)
@@ -117,7 +117,7 @@ def print_log_acc_bwt(args, acc, lss):
     logs['rij'] = acc[-1]
 
     # pickle
-    path = os.path.join(args.checkpoint, '{}_{}_seed_{}.p'.format(args.experiment,args.trainer, args.seed))
+    path = os.path.join(args.checkpoint, '{}_{}_seed_{}.p'.format(args.experiment,args.train_mode, args.seed))
     with open(path, 'wb') as output:
         pickle.dump(logs, output)
 